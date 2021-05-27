84405dc945589246d06f4352080afd4ee6e9c6df
Brandon Meilleur <brandon.meilleur@boldcommerce.com>
2021-05-27 15:52:17 -0500

    fix missing file from last commit

--- src/helpers/helpers.go
+++ src/helpers/helpers.go
@@ -11,38 +11,55 @@ func FailOnError(err error, msg string) {
 	}
 }
 
-func SetupQueue(ch *amqp.Channel, exchangeName, exchangeKind, deadLetterExchangeName, queueName, routingKey string) (amqp.Queue, error) {
+func SetupQueueBinding(ch *amqp.Channel, exchangeName, exchangeKind, queueName, routingKey, deadLetterExchangeName string, durableQueue bool) amqp.Queue {
 	err := ch.ExchangeDeclare(
-		exchangeName, // name
-		exchangeKind,      // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
+		exchangeName,
+		exchangeKind,
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
 	FailOnError(err, "Failed to declare an exchange")
 
-	textQ, err := ch.QueueDeclare(
-		queueName, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		amqp.Table{"x-dead-letter-exchange": deadLetterExchangeName},     // arguments
+	var qArgs amqp.Table
+	if deadLetterExchangeName != "" {
+		qArgs = amqp.Table{"x-dead-letter-exchange": deadLetterExchangeName}
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName,
+		durableQueue,
+		false,
+		false,
+		false,
+		qArgs,
 	)
 	FailOnError(err, "Failed to declare a queue")
 
-	err = ch.QueueBind(textQ.Name,       // queue name
-		routingKey,            // routing key
-		exchangeName, // exchange
+	err = ch.QueueBind(
+		q.Name,
+		routingKey,
+		exchangeName,
 		false,
-		nil)
+		nil,
+	)
 	FailOnError(err, "Failed to bind a queue")
 
-	if err != nil {
-		return amqp.Queue{}, err
-	}
+	return q
+}
 
-	return textQ, nil
+func PublishMessage(ch *amqp.Channel, exchangeName, routingKey, contentType, body string) {
+	err := ch.Publish(
+		exchangeName,
+		routingKey,
+		false,
+		false,
+		amqp.Publishing {
+			ContentType: contentType,
+			Body:        []byte(body),
+		})
+	FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s for %s", routingKey, body)
 }
